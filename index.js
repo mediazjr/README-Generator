146f8db391cb520243ef3e46b0e840d686ca6fdb
mediazjr <91097193+mediazjr@users.noreply.github.com>
2021-12-16 20:22:37 -0800

    added array of questions for input

--- index.js
+++ index.js
@@ -4,7 +4,138 @@ const fs = require('fs');
 const generateMarkdown = require("./utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+        type: 'input',
+        question: 'What is the title of your project?',
+        name: 'title',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter the title of your project!');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        question: 'Please enter a description of your project.',
+        name: 'description',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter the description of your project!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        question: 'Please enter the installation instructions.',
+        name: 'installation',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter the installation instructions of your project!');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        question: 'Please enter the usage information of your project.',
+        name: 'usage',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter the usage information of your project!');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        question: 'Please enter the contribution guidelines of your project.',
+        name: 'contribution',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter the contribution guidelines of your project!');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        question: 'Please enter the test instructions of your project.',
+        name: 'test',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter the test instruction of your project!');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'list',
+        question: 'Which license did you use?',
+        name: 'license',
+        choices: ['Apache_2.0', 'BSD_2--Clause', 'CC0_1.0', 'MIT', 'MPL_2.0'],
+    },
+
+    {
+        type: 'input',
+        question: 'Please enter any additional questions.',
+        name: 'questions',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter any additional questions for your project!');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        question: 'What is your GitHub username?',
+        name: 'username',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username!');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        question: 'What is your email address?',
+        name: 'email',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your email address!');
+                return false;
+            }
+        }
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
